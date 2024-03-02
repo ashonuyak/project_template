494b1e23847803170c4a403559006f9e65a6bc30
ashonuyak <ashonuyak@gmail.com>
2024-03-02 20:23:16 +0200

    lab3: initial

--- hub/app/adapters/store_api_adapter.py
+++ hub/app/adapters/store_api_adapter.py
@@ -1,8 +1,6 @@
 import json
-import logging
 from typing import List
 
-import pydantic_core
 import requests
 
 from app.entities.processed_agent_data import ProcessedAgentData
@@ -14,11 +12,6 @@ class StoreApiAdapter(StoreGateway):
         self.api_base_url = api_base_url
 
     def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
-        """
-        Save the processed road data to the Store API.
-        Parameters:
-            processed_agent_data_batch (dict): Processed road data to be saved.
-        Returns:
-            bool: True if the data is successfully saved, False otherwise.
-        """
-        # Implement it
+        data = [processed_agent_data.model_dump_json() for processed_agent_data in processed_agent_data_batch]
+        response = requests.post(f"{self.api_base_url}/processed_agent_data/", data='[' + ','.join(data) + ']', headers={'Content-Type': 'application/json'})
+        return response.status_code == 200
